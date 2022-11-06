377fe362414fad4dc6e6a1f3db5afd1e0a81cab4
Hugo Jeller Ferreira <hugo_jeller@hotmail.com>
2022-11-06 02:33:07 -0400

    Polish code

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,64 @@ const PROGRAM_MEMORY = 512;
 const DISPLAY_WIDTH = 64;
 const DISPLAY_HEIGHT = 32;
 
-const codes = {
+function deepClone(subject: any) {
+    return JSON.parse(JSON.stringify(subject));
+}
+
+function dec2bin(dec: number) {
+    return dec.toString(2).padStart(8, '0');
+}
+
+function rand() {
+    return Math.floor(256 * Math.random());
+}
+
+function matrix(x: number, y: number) {
+    return Array(x).fill(0).map(() => Array(y).fill(0))
+}
+
+const opcodes = {
+    'ANNN': (nnn: number) => i = nnn,
+    'CXNN': (x: number, nn: number) => v[x] = rand() & nn,
+    '3XNN': (x: number, nn: number) => {
+        if (v[x] === nn) {
+            pc += 2
+        }
+    },
+    'DXYN': (x: number, y: number, n: number) => {
+        const vx = v[x];
+        const vy = v[y];
+
+        for (let b = 0; b < n; b++) {
+            const byte = parseInt(memory[i + b], 16);
+            const bits = dec2bin(byte);
+            for (let a = 0; a < 8; a++) {
+                const xx = vx + a;
+                const yy = vy + b;
+                const set = bits[a] === '1';
+                if (set) {
+                    display[xx % DISPLAY_WIDTH][yy % DISPLAY_HEIGHT] = true;
+                }
+            }
+        }
+    },
+    '7XNN': (x: number, nn: number) => v[x] += nn,
+    '1NNN': (nnn: number) => pc = nnn,
+    '6XNN': (x: number, nn: number) => v[x] = nn,
+    '8XY0': (x: number, y: number) => v[x] = v[y],
+    '2NNN': (nnn: number) => {
+        stack.push(pc);
+        pc = nnn;
+    }
+}
+
+interface Instruction {
+    instruction: string,
+    opcode: keyof typeof opcodes,
+    parameters: number[],
+}
+
+const codes: Record<string, string[]> = {
     maze: [
         'a21e', 'c201', '3201', 'a21a', 'd014', '7004', '3040', '1200',
         '6000', '7104', '3120', '1200', '1218', '8040', '2010', '2040',
@@ -20,72 +77,14 @@ const codes = {
 }
 
 const code = codes.mazeAlt.map(instruction => instruction.toUpperCase());
-
 let i = 0;
 let pc = PROGRAM_MEMORY;
 const memory = Array(4096).fill(0);
 const stack = [];
-const vx = Array(8).fill(0);
+const v = Array(8).fill(0);
 const sp = () => stack.length;
-const display = Array(64).fill(0).map(() => Array(32).fill(false));
 
-function dec2bin(dec: number) {
-    return dec.toString(2).padStart(8, '0');
-}
-
-function rand() {
-    return Math.floor(256 * Math.random());
-}
-
-const opcodes = {
-    'ANNN': (nnn: number) => i = nnn,
-    'CXNN': (x: number, nn: number) => vx[x] = rand() & nn,
-    '3XNN': (x: number, nn: number) => {
-        if (vx[x] === nn) {
-            pc += 2
-        }
-    },
-    'DXYN': (x: number, y: number, n: number) => {
-        const _vx = vx[x];
-        const _vy = vx[y];
-
-        for (let b = 0; b < n; b++) {
-            const byte = parseInt(memory[i + b], 16);
-            const bits = dec2bin(byte);
-            for (let a = 0; a < 8; a++) {
-                const xx = _vx + a;
-                const yy = _vy + b;
-                const set = bits[a] === '1';
-                if (set) {
-                    display[xx % DISPLAY_WIDTH][yy % DISPLAY_HEIGHT] = true;
-                }
-
-                console.log('rendering', {
-                    a,
-                    b,
-                    i,
-                    x,
-                    y,
-                    n,
-                    result: bits[a] === '0',
-                    byte,
-                    memory,
-                    bit: bits[b],
-                    bits,
-                    display
-                })
-            }
-        }
-    }, // TODO: WTF
-    '7XNN': (x: number, nn: number) => vx[x] += nn,
-    '1NNN': (nnn: number) => pc = nnn,
-    '6XNN': (x: number, nn: number) => vx[x] = nn,
-    '8XY0': (x: number, y: number) => vx[x] = vx[y],
-    '2NNN': (nnn: number) => {
-        stack.push(pc);
-        pc = nnn;
-    }
-}
+const display = matrix(64, 32);
 
 function parameterToMatcher(parameter: string) {
     return `(.{${parameter.length}})`;
@@ -93,17 +92,6 @@ function parameterToMatcher(parameter: string) {
 
 const parameters = ['X', 'Y', 'NNN', 'NN', 'N'];
 const parameterMatchers = parameters.map(parameterToMatcher);
-const instructions = [
-    'ANNN',
-    'CXNN',
-    '3XNN',
-    'DXYN',
-    '7XNN',
-    '1NNN',
-    '6XNN',
-    '8XY0',
-    '2NNN',
-]
 
 function instructionToMatcher(instruction: string) {
     parameterMatchers.forEach((matcher, index) => {
@@ -113,6 +101,7 @@ function instructionToMatcher(instruction: string) {
     return instruction;
 }
 
+const instructions = Object.keys(opcodes);
 const instructionMatcher = instructions.map(instructionToMatcher)
 
 function loadToMemory(code: any[]) {
@@ -122,105 +111,61 @@ function loadToMemory(code: any[]) {
     })
 }
 
-console.log(instructionMatcher)
+function disassemble(rawInstruction: string): Instruction {
+    console.assert(rawInstruction.length === 4, `Instruction ${rawInstruction} is not 4 chars long`);
 
-function disassemble(rawInstruction: string) {
-    console.log(`Disassembling ${rawInstruction}`)
     const matches = instructionMatcher.map((matcher, index) => {
         const match = rawInstruction.match(new RegExp(matcher));
         if (match) {
             return {
-                instruction: instructions[index],
-                parts: match,
+                instruction: match[0],
+                opcode: instructions[index],
+                parameters: match.splice(1).map(parameter => parseInt(parameter, 16)),
             }
         }
     });
+
     const filteredMatches = matches.filter(Boolean);
-    if (filteredMatches.length !== 1) {
-        throw new Error(`Error while disassembling instruction ${rawInstruction}`);
-    }
+    console.assert(filteredMatches.length === 1, `Expected only 1 match for instruction ${rawInstruction}, received ${filteredMatches}`);
 
-    const match = filteredMatches[0];
-
-    return {rawInstruction, match};
-}
-
-function vblank() {
-    for (let x = 0; x < 64; x++) {
-        for (let y = 0; y < 32; y++) {
-            display[x][y] = false;
-        }
-    }
+    return filteredMatches[0] as Instruction;
 }
 
 function step() {
-    // vblank();
-    const raw = memory[pc] + memory[pc + 1];
-    pc += 2;
+    const raw = memory[pc++] + memory[pc++];
     const instruction = disassemble(raw);
 
-    console.log(instruction)
+    const opcode = opcodes[instruction.opcode];
 
     // @ts-ignore
-    const opcode = opcodes[instruction.match.instruction];
-
-    const params = instruction?.match?.parts.splice(1).map(raw => parseInt(raw, 16));
-    console.log({params})
-    // @ts-ignore
-    opcode(...params);
-    console.log({
-        i, pc, sp, memory, vx,
-    })
+    opcode(...instruction.parameters);
 }
 
+loadToMemory(code);
+
 function App() {
-    const scale = 15;
-    const [render, setRender] = useState<number[][]>(Array(64).fill(0).map(() => Array(32)));
-    const [offset, setOffset] = useState(0);
+    const [render, setRender] = useState(matrix(64, 32));
 
     function stepAndRender() {
         step();
-        setRender([...display])
+        setRender(deepClone(display))
     }
 
     useInterval(() => {
-        // memory[600] = 0xf0;
-        // memory[601] = 0x90;
-        // memory[602] = 0x90;
-        // memory[603] = 0x90;
-        // memory[604] = 0xf0;
-        // i = 600;
-        // vx[0] = offset;
-        // vx[1] = 10;
-        // vblank();
-        // opcodes['DXYN'](0, 1, 5);
         stepAndRender();
-        // setOffset(offset + 1);
     }, 20)
 
-    // useInterval(() => console.clear(), 5000);
-    loadToMemory(code);
-    return <div>
-        <button onClick={() => stepAndRender()}>step</button>
-        <div style={{position: 'relative'}}>
-            {
-                render.map((column, x) => (
-                    column.map((value, y) => (
-                        <div key={['x', x, 'y', y].join()} style={{
-                            top: `${y * scale}px`,
-                            left: `${x * scale}px`,
-                            width: `${scale}px`,
-                            height: `${scale}px`,
-                            position: 'absolute',
-                            background: render[x][y] ? 'orange' : 'gray'
-                        }}>
-                        </div>
-                    ))
-                ))
-
-            }
-
-        </div>
+    return <div className="display">
+        {render.map((column, x) => (
+            <div className="display__column">
+                {column.map((value, y) => (
+                    <div
+                        key={['x', x, 'y', y].join()}
+                        className={`display__pixel ${render[x][y] ? 'active' : 'inactive'}`}
+                    ></div>
+                ))}
+            </div>
+        ))}
     </div>
 }
 
